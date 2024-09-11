14afd146d763382e7404a647817c507e1e235c66
james-lomax <james@citymapper.com>
2024-09-11 14:13:30 +0100

    Broken correctly

--- app/src/main/java/com/example/edittextbugmini/FormTextField.kt
+++ app/src/main/java/com/example/edittextbugmini/FormTextField.kt
@@ -1,29 +1,41 @@
 package com.example.edittextbugmini
 
 import android.text.InputType
+import android.view.View
 import android.widget.EditText
 import android.widget.LinearLayout
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.relocation.BringIntoViewRequester
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.semantics.insertTextAtCursor
 import androidx.compose.ui.semantics.requestFocus
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.setText
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 public fun FormTextFieldComponent() {
+    val (bringIntoViewRequester, onFocusChangeListener) = rememberBringIntoViewInterop()
+
     var editText by remember { mutableStateOf<EditText?>(null) }
 
     Box(
@@ -66,14 +78,14 @@ public fun FormTextFieldComponent() {
 
                     setBackgroundColor(Color.Transparent.toArgb())
 
-//                setOnFocusChangeListener { v, hasFocus ->
-//                    onFocusChangeListener.onFocusChange(v, hasFocus)
-////                    if (!hasFocus) {
-////                        updatedOnUnfocus()
-////                        setSelection(0)
-////                    }
-////                    fieldHasFocus = hasFocus
-//                }
+                    setOnFocusChangeListener { v, hasFocus ->
+                        onFocusChangeListener.onFocusChange(v, hasFocus)
+    //                    if (!hasFocus) {
+    //                        updatedOnUnfocus()
+    //                        setSelection(0)
+    //                    }
+    //                    fieldHasFocus = hasFocus
+                    }
                 }
 
                 LinearLayout(context).apply {
@@ -90,4 +102,28 @@ public fun FormTextFieldComponent() {
             }
         )
     }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun rememberBringIntoViewInterop(): Pair<BringIntoViewRequester, View.OnFocusChangeListener> {
+    val bringIntoViewRequester = remember { BringIntoViewRequester() }
+    val coroutineScope = rememberCoroutineScope()
+    var inputIsFocused by remember { mutableStateOf(false) }
+
+    // When the keyboard appears, we need to re-triggered bringIntoView
+    LaunchedEffect(WindowInsets.ime.getBottom(LocalDensity.current)) {
+        if (inputIsFocused) {
+            bringIntoViewRequester.bringIntoView()
+        }
+    }
+
+    return Pair(bringIntoViewRequester, View.OnFocusChangeListener { _, hasFocus ->
+        inputIsFocused = hasFocus
+        if (hasFocus) {
+            coroutineScope.launch {
+                bringIntoViewRequester.bringIntoView()
+            }
+        }
+    })
 }
